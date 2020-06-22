c18812111cc54e6e36f746c46531af0520388750
toniuyt123 <antoniomilev123@gmail.com>
2020-06-22 20:54:06 +0600

    'drawing'

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ function preload() {
 
 const rad = Math.PI / 180;
 
-
 function matrix(a, b, c, d, e, f) {
     const m = mat2d.fromValues(a, b, c, d, e, f);
     return m;
@@ -24,6 +23,9 @@ const T3 = matrix(1/3 * Math.cos(-60* rad), -1/3 * Math.sin(-60* rad),
 
 const T4 = matrix(1/3 * Math.cos( 0 * rad), -1/3 * Math.sin( 0 * rad),
                   1/3 * Math.sin( 0 * rad),  1/3 * Math.cos( 0 * rad), 2/3, 0);
+
+const matrices = [T1, T2, T3, T4];
+let iterations = 2;
 
 function drawWithMatrix(t) {
     push();
@@ -46,6 +48,61 @@ function setup() {
     translateX = width / 2;
     translateY = height / 2;
 
+    setupDrawing();
+
+    $('#fractal-iterations').on('input', function () {
+        iterations = $('#fractal-iterations').val();
+        draw();
+    });
+}
+
+function iterate(mcs, depth) {
+    for (const m1 of mcs) {
+        const new_matrices = [];
+        for (const m2 of matrices) {
+            new_matrices.push(mul(m1, m2));
+        }
+        if (depth > 0) {
+            iterate(new_matrices, depth - 1);
+        } else {
+            drawWithMatrix(m1);
+        }
+
+    }
+}
+
+function draw() {
+    background(0);
+    scale(1, 1);
+    translate(translateX, translateY);
+    scale(scaleFactor);
+
+    iterate(matrices, iterations)
+}
+
+let scaleFactor = 1.0;
+let translateX = 0.0;
+let translateY = 0.0;
+let zoomSensitivity = 0.001;
+
+function mouseWheel(event) {
+    event.preventDefault();
+    translateX -= mouseX;
+    translateY -= mouseY;
+    let delta = 1 - event.delta * zoomSensitivity;
+    scaleFactor *= delta;
+    translateX *= delta;
+    translateY *= delta;
+    translateX += mouseX;
+    translateY += mouseY;
+}
+
+function mouseDragged() {
+    translateX += mouseX - pmouseX;
+    translateY += mouseY - pmouseY;
+}
+
+function setupDrawing() {
     var drawerPlugins = [
         // Drawing tools
         'Pencil',
@@ -80,20 +137,9 @@ function setup() {
         'MinimizeButton',
         'ToggleVisibilityButton',
         'MovableFloatingMode',
-        'FullscreenModeButton',
-
-        'TextLineHeight',
-        'TextAlign',
-
-        'TextFontFamily',
-        'TextFontSize',
-        'TextFontWeight',
-        'TextFontStyle',
-        'TextDecoration',
-        'TextColor',
-        'TextBackgroundColor'
     ];
 
+    
     var drawingCanvas = new DrawerJs.Drawer(null, {
         plugins: drawerPlugins,
         corePlugins: [
@@ -107,9 +153,8 @@ function setup() {
             },
             BackgroundImage: {
                 scaleDownLargeImage: true,
-                maxImageSizeKb: 10240, //1MB
-                //fixedBackgroundUrl: '/examples/redactor/images/vanGogh.jpg',
-                imagePosition: 'center',  // one of  'center', 'stretch', 'top-left', 'top-right', 'bottom-left', 'bottom-right'
+                maxImageSizeKb: 10240, 
+                imagePosition: 'center',
                 acceptedMIMETypes: ['image/jpeg', 'image/png', 'image/gif'] ,
                 dynamicRepositionImage: true,
                 dynamicRepositionImageThrottle: 100,
@@ -187,9 +232,11 @@ function setup() {
             saveInHtml: false,
             saveImageData: function(canvasId, imageData) {
                 console.log(imageData);
+                texture = loadImage(imageData);
+                draw();
             }
         },
-         defaultImageUrl: 'images/drawer.jpg',
+        defaultImageUrl: 'images/drawer.jpg',
         defaultActivePlugin : { name : 'Pencil', mode : 'lastUsed'},
         debug: true,
         activeColor: '#a1be13',
@@ -199,53 +246,7 @@ function setup() {
         imagesContainer: '#image-container',
     }, 400, 400);
 
+
     $('#drawing-container').append(drawingCanvas.getHtml());
     drawingCanvas.onInsert();
-}
-
-const matrices = [T1, T2, T3, T4];
-
-function iterate(mcs, depth) {
-    for (const m1 of mcs) {
-        const new_matrices = [];
-        for (const m2 of matrices) {
-            new_matrices.push(mul(m1, m2));
-        }
-        if (depth > 0) {
-            iterate(new_matrices, depth - 1);
-        } else {
-            drawWithMatrix(m1);
-        }
-
-    }
-}
-
-function draw() {
-    background(0);
-    scale(1, 1);
-    translate(translateX, translateY);
-    scale(scaleFactor);
-
-    iterate(matrices, 5)
-}
-
-let scaleFactor = 1.0;
-let translateX = 0.0;
-let translateY = 0.0;
-let zoomSensitivity = 0.001;
-function mouseWheel(event) {
-    event.preventDefault();
-    translateX -= mouseX;
-    translateY -= mouseY;
-    let delta = 1 - event.delta * zoomSensitivity;
-    scaleFactor *= delta;
-    translateX *= delta;
-    translateY *= delta;
-    translateX += mouseX;
-    translateY += mouseY;
-}
-
-function mouseDragged() {
-    translateX += mouseX - pmouseX;
-    translateY += mouseY - pmouseY;
 }
